267ddafbcdc7c939fedb71c80eb5acb3c8bc70dc
Philipp Drebes <philipp.drebes@softec.ch>
2024-10-21 09:59:32 +0200

    init

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # jsonpath.nvim
+
+## Setup
+
+### lazy.nvim
+
+```lua
+return {
+	"philippdrebes/jsonpath.nvim",
+	dependencies = { "nvim-treesitter/nvim-treesitter" },
+	config = function()
+		local jsonpath = require("jsonpath")
+
+		jsonpath.setup()
+
+        vim.keymap.set("n", "<leader>jp", function() jsonpath.show_json_path() end, { desc = "Show JSON Path" })
+		vim.keymap.set("n", "<leader>jpy", function() jsonpath.yank_json_path() end, { desc = "Yank JSON Path" })
+	end,
+}
+```
+
---
+++ lua/jsonpath.lua
@@ -0,0 +1,72 @@
+local ts_utils = require("nvim-treesitter.ts_utils")
+local M = {}
+
+local function get_json_path(bufnr)
+	local node = ts_utils.get_node_at_cursor()
+	if not node then
+		return "No Treesitter node found"
+	end
+
+	local path = {}
+	while node do
+		local node_type = node:type()
+		if node_type == "pair" then
+			local key_node = node:field("key")[1]
+			if key_node then
+				local key_name = vim.treesitter.get_node_text(key_node, bufnr)
+				key_name = key_name:gsub('"', "")
+				table.insert(path, 1, key_name)
+			end
+		end
+		node = node:parent()
+	end
+
+	if #path == 0 then
+		return "Cursor is not inside a JSON object"
+	end
+
+	return table.concat(path, ".")
+end
+
+function M.show_json_path()
+	local bufnr = vim.api.nvim_get_current_buf()
+	local json_path = get_json_path(bufnr)
+
+	local width = math.ceil(string.len(json_path))
+	local height = 1
+	local opts = {
+		relative = "cursor",
+		width = width,
+		height = height,
+		row = 1,
+		col = 0,
+		style = "minimal",
+		border = "single",
+	}
+
+	local buf = vim.api.nvim_create_buf(false, true)
+	vim.api.nvim_buf_set_lines(buf, 0, -1, false, { json_path })
+	local win = vim.api.nvim_open_win(buf, false, opts)
+
+	vim.api.nvim_create_autocmd("CursorMoved", {
+		buffer = bufnr,
+		callback = function()
+			if vim.api.nvim_win_is_valid(win) then
+				vim.api.nvim_win_close(win, true)
+			end
+		end,
+	})
+end
+
+function M.yank_json_path()
+	local bufnr = vim.api.nvim_get_current_buf()
+	local json_path = get_json_path(bufnr)
+	vim.fn.setreg('"', json_path)
+	vim.api.nvim_echo({ { "Yanked JSON Path: " .. json_path, "Normal" } }, false, {})
+end
+
+function M.setup(opts)
+	opts = opts or {}
+end
+
+return M
